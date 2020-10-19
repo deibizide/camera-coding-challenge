c5f8474d9862c48a55e65d4a0c63da7345f17195
deibizide <david.ramis89@gmail.com>
2020-10-19 08:20:30 +0200

    increase file limit size

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ app.get('/*', (req, res) => {
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const port = process.env.PORT || 8050; // set our port
 
